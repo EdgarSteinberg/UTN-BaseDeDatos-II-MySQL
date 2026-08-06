a8203f6ec302ab7b5d80a057e3a23385b288edc7
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-08-06 16:44:27 -0300

    Práctica de JOINs mediante claves foráneas

--- DML/selects.sql
+++ DML/selects.sql
@@ -12,8 +12,109 @@ SELECT Title, Release_Year FROM contents;
 SELECT Email FROM clients;
 
 
--- Filtrado avanzado
+-- ==========================================
+-- CONSULTAS BÁSICAS (SELECT)
+-- ==========================================
 
-SELECT Title, Release_Year FROM contents
-WHERE Release_Year
-BETWEEN 2018 AND 2023 AND idContents_Types IN(1,2);
+
+SELECT * FROM contents;
+
+SELECT
+	* 
+FROM 
+	contents c 
+WHERE 
+	c.Release_Year > 2000;
+
+
+-- ==========================================
+-- JOIN CLÁSICO (Sintaxis antigua)
+-- ========================================== 
+
+
+SELECT
+	* 
+FROM 
+	contents c , contents_types ct
+WHERE 
+	c.Release_Year > 2000
+and
+    c.idContents_Types = ct.idContents_Types
+;
+
+-- union de dos tablas con el where
+SELECT
+	ct.Name content_type,
+    c.Title , 
+    c.Release_Year
+FROM 
+	contents c , contents_types ct
+WHERE 
+	c.Release_Year > 2000
+and
+    c.idContents_Types = ct.idContents_Types
+;
+
+
+-- ==========================================
+-- INNER JOIN
+-- ==========================================
+
+
+SELECT
+	ct.Name content_type,
+    c.Title,
+    c.Release_Year
+FROM
+	contents c
+INNER JOIN
+	contents_types ct
+ON
+	c.idContents_Types = ct.idContents_Types
+WHERE
+	c.Release_Year > 2000
+;
+
+-- ==========================================
+-- INNER JOIN ENTRE TRES TABLAS
+-- ==========================================
+
+
+SELECT
+	ct.Name content_type,
+    c.Title,
+    c.Release_Year,
+    cat.Name  categories
+FROM
+	contents c
+INNER JOIN
+	contents_types ct
+ON
+	c.idContents_Types = ct.idContents_Types
+INNER JOIN
+	categories cat
+ON
+	c.idCategories = cat.idCategories
+WHERE
+	c.Release_Year > 2000
+ORDER BY
+	c.Release_Year DESC
+;
+
+-- ==========================================
+-- AGRUPAMIENTO (GROUP BY + HAVING)
+-- ==========================================
+
+SELECT
+    COUNT(c.idContents) AS cantidad,
+    c.Release_Year
+FROM 
+    contents c
+WHERE 
+    c.Release_Year > 2000
+GROUP BY
+     c.Release_Year
+HAVING 
+    COUNT(c.idContents) > 1
+ORDER BY 
+    c.Release_Year ASC;
